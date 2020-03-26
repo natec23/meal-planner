f4f5cd64a0ee41e2b4735ae69c5e80dd26b89527
Nate Crawford <natec23@gmail.com>
2020-03-25 20:36:37 -0500

    default laravel auth setup

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes(['register' => false]);
+
+Route::get('/home', 'HomeController@index')->name('home');
